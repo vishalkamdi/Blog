65aeafe92480ff2384048c0dff52b28a6430b5f3
vishal kamdi <vishalkamdi2901@gmail.com>
2024-01-23 16:24:59 +0530

    update blog project

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom'
+
+export default function Protected({children,authentication =true}) {
+
+    const navigate =useNavigate();
+    const  [loader ,setLoader]=useState(true)
+    const authStatus = useSelector(state => state.auth.status)
+
+    useEffect(()=>{
+      
+        if (authentication && authStatus !==authentication) {
+            navigate("/login")
+        }else if(!authentication && authStatus !==authentication){
+            navigate("/")
+        }
+        setLoader(false)
+
+    }, [authStatus,navigate,authentication])
+     
+  return (
+    <div>AuthLayout</div>
+  )
+}
+
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,15 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+
+export{
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE
+}
